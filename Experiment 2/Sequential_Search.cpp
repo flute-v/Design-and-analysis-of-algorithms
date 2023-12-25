9f163043ef3459a59b2941a88e212e678e50716e
Venu Virparia <129399359+flute-v@users.noreply.github.com>
2023-12-25 21:49:27 +0530

    Update Sequential_Search.cpp

--- Experiment 2/Sequential_Search.cpp
+++ Experiment 2/Sequential_Search.cpp
@@ -1,43 +1,30 @@
+#include <bits/stdc++.h>
 #include <iostream>
 #include <time.h>
-#include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr[], int l, int r, int x)
+int search(int arr[], int N, int x)
 {
-	if (r >= l) {
-		int mid = l + (r - l) / 2;
-
-		
-		if (arr[mid] == x)
-			return mid;
-
-		if (arr[mid] > x)
-			return binarySearch(arr, l, mid - 1, x);
-
-		return binarySearch(arr, mid + 1, r, x);
-	}
-
-	
-	return -1;
+    for (int i = 0; i < N; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
 }
 
-int main()
+int main(void)
 {
-	
-    int x = 1;
-    int N = 10;
+    int x = 500000;
+    int N = 1000000;
     int a[N];
     clock_t t = clock();
 
     for (int i = 1; i <= N; i++)
         a[i] = i;
-    int result = binarySearch(a, 0, N - 1, x);
-
+        
+    int result = search(a, N, x);
     t = clock() - t;
     double time_taken = ((double)t) / CLOCKS_PER_SEC;
     cout << time_taken;
-
-
-	return 0;
+    
+    return 0;
 }
