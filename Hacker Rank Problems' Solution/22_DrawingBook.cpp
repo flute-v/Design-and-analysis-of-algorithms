714d085e69d986ce7afba32582423ffb086586c1
Venu Virparia <129399359+flute-v@users.noreply.github.com>
2024-03-04 23:21:58 +0530

    Create 22_DrawingBook.cpp

---
+++ Hacker Rank Problems' Solution/22_DrawingBook.cpp
@@ -0,0 +1,78 @@
+//Drawing Book
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+int pageCount(int n, int p) {
+    if((n%2==0 && n-p==1 && n!=2))
+        return 1; 
+    else{
+        int b=0, e=0; 
+        e = (n-p)/2;
+        if(p%2==0)
+            b = p/2;
+        else
+            b = (p-1)/2;
+        if(b<e)
+            return b;
+        else 
+            return e;       
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string p_temp;
+    getline(cin, p_temp);
+
+    int p = stoi(ltrim(rtrim(p_temp)));
+
+    int result = pageCount(n, p);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
